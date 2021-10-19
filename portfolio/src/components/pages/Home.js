48f54288dd200216f338461e978cb0b008d7209e
RJR <80534043+rjr2@users.noreply.github.com>
2021-10-19 10:48:56 -0400

    File Structure, home and portfolio

---
+++ portfolio/src/components/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <article id="welcome" class="container shadow p-3 mb-3 rounded">
+      <h3 class="text-center">Welcome!</h3>
+        <p>Welcome to my Portfolio!</p>
+        <p>
+          I am an aspiring fullstack developer currently studying at the University of Richmond. I am a past graduate of Virginia Commonwealth University with a Bachelor's degree in English. I have spent most of my life in public service; serving my local communities as a Paramedic for over a decade, and as a former soldier in the US Army.
+        </p>
+        <p>
+          I take a lot of creative inspiration from my outdoor hobbies. When I am not in front of my computer I can either be found on a backroad somewhere in the mountains exploring new sights, or on my kayak floating down the river watching birds and taking in the views of untouched nature.
+        </p>
+    </article>
+  );
+}
---
+++ portfolio/src/components/pages/Portfolio.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Card1Pic from '../../images/calendontsh.png'
+import Card2Pic from '../../images/dpscreen.png'
+import Card3Pic from '../../images/pwscreen.png'
+
+export default function Portfolio() {
+  return (
+    <article id="portfolio" class="portfolio container shadow p-3 mb-3 rounded">
+            <h3 class="text-center">Portfolio</h3>
+            <div class="row justify-content-center">
+                <div class="col text-center">
+                    <div class="card shadow p-3 mb-3 rounded text-center" style={{width: '18rem'}}>
+                        <a class="portfolio" href="https://pacific-badlands-48977.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                            <img src={Card1Pic} class="card-img-top" alt="screenshot of Calendont" />
+                            <div class="card-body">
+                                <p class="card-text text-center">A project that can help you make and cancel plans!</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>  
+                <div class="col text-center">
+                    <div class="card shadow p-3 mb-3 rounded" style={{width: '18rem'}}>
+                        <a class="portfolio" href="https://rjr2.github.io/dayplanner/" target="_blank" rel="noopener noreferrer">
+                            <img src={Card2Pic} class="card-img-top" alt="Screenshot of day planner" />
+                            <div class="card-body">
+                                <p class="card-text text-center">A simple dayplanner that uses local storage to organize your day!</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>  
+                <div class="col text-center">
+                    <div class="card shadow p-3 mb-3 rounded" style={{width: '18rem'}}>
+                        <a class="portfolio" href="https://rjr2.github.io/PasswordGenerator/" target="_blank" rel="noopener noreferrer">
+                            <img src={Card3Pic} class="card-img-top" alt="Screenshot of password generator" />
+                            <div class="card-body">
+                                <p class="card-text text-center">A password generator for when you need to be extra secure.</p>
+                            </div>
+                        </a>
+                    </div>
+                </div>  
+            </div>
+        </article>
+  );
+}
